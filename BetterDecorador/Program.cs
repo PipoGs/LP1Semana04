6147227f812db0dbcd62dafcbc876f191a818c2a
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-12 19:38:29 +0000

    Added cicle for to add decorations

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -6,12 +6,20 @@ namespace BetterDecorador
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(Decor("Hello LP1!", '*'));
+            char character = char.Parse(args[1]);
+            int number = int.Parse(args[2]);
+
+            Console.WriteLine(Decor(args[0], character, number));
         }
 
-        private static string Decor(string s, char dec)
+        private static string Decor(string s, char dec, int n)
         {
-            return "{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+            string str = Convert.ToString(dec);
+            for (int i = n - 1; i > 0; i--)
+            {
+                str += dec;
+            }
+            return $"{str} {s} {str}";
         }
     }
 }
