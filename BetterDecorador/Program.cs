06a3a5d80d1644e1bfce8cde61eeda64a397126b
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-12 19:55:46 +0000

    Added XML comments

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,6 +4,12 @@ namespace BetterDecorador
 {
     public class Program
     {
+
+        /// <summary>
+        /// Main method that receives the argument and calls Decor method
+        /// to decorate the string with the character and number of times
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             char character = char.Parse(args[1]);
@@ -12,6 +18,14 @@ namespace BetterDecorador
             Console.WriteLine(Decor(args[0], character, number));
         }
 
+        /// <summary>
+        /// This Method receives a string, a character and a number and returns
+        /// the string decorated with the character and number of times
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int n)
         {
             string str = Convert.ToString(dec);
