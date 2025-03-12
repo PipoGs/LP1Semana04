455cbe090aacbcff00a49407b4677a2361dec641
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-12 19:05:42 +0000

    added conversion

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Hello LP1!", '*'));
+        }
+
+        private static string Decor(string s, char dec)
+        {
+            return "{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
+    }
+}
