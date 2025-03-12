0dfe9c75477c36e2a081e5c47b21491a6024bd15
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-12 22:22:18 +0000

    Added first tries for code

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,25 @@ namespace Special
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Insert n-th number of the special sequence: ");
+            
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetSpecial(n));
+        }
+
+        private static int GetSpecial(int n)
+        {
+            int firstnum = 0;
+            int secondnum = 1;
+            int result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                result = 2 * firstnum + secondnum; 
+            }
+            
+            return  result;
         }
     }
 }
